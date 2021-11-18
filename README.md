1433863b480b33516bebf8f672afcfc2ec363451
Nandins-12 <fernandogdias12@gmail.com>
2021-11-18 09:44:39 -0300

    Added comments and README

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1 align="center">App Orçamento Pessoal 📃</h1>
+
+## Sobre o desenvolvimento 🔍
+Projeto feito para fins didáticos e prática da linguagem JavaScript. O desenvolvimento desse projeto foi feito usando as tecnologias:
+
+### Tecnologias: <h3> 
+* HTML
+* CSS - Criação de estilos e costumizações.
+
+### Linguagem de programação: <h3>
+* JavaScript - Programar o app atráves de scripts externos.
+
+>Observações: O foco deste projeto foi o treinamento e prática da linguagem JavaScript.
+
+## Sobre o App 💻
+
+O seu objetivo é claro, este app é uma interface para gerenciar suas financias, com um card com o calculo dos gastos (saidas), um card para o calculo das entradas e por ultimo um card para o calculo do total.
+
+Ao cliclar em '*nova transação*', será exibido um modal com campos de: descrição, valor e data a ser preenchidos, para o cadastramento de uma nova transação.
+
+O banco de dados utilizado é o localStorage, no qual é armazenado as transações.
+
+A sua interface é apresentada de uma maneira simples pôrem dinamica e de boa visualização, para que seja de facil absorção para com os usuários.
+
+## Previews
+
+<div align="center">
+    <div><img src="assets/previews/preview1.png" width="700"></div>
+    <div><img src="assets/previews/preview3.png" width="200"></div>
+    <div><img src="assets/previews/preview2.png" width="700"></div>
+    
+</div>
--- index.js
+++ index.js
@@ -1,35 +1,38 @@
+// Objeto literal responsavel pelo controle do Modal.
 const Modal = {
     modal: document.querySelector('.modal-overlay'),
+
+    //Método responsavel por atribuir a classe 'active' ao Modal e deixa-lo visível.
     toggle() {
-        if (this.modal.classList.contains('active')) {
-            this.modal.classList.remove('active')
-        } else {
-            this.modal.classList.add('active')
-        }
+        this.modal.classList.toggle('active')
     }
 }
 
+//Objeto literal que armazena alguns métodos que serão utéis ao decorrer do script.
 const Utils = {
+    //Formata o valor capturado do DOM
     formatAmount(value) {
-        value = Number(value) * 100
+        value = /*Método que transforma o valor em número*/Number(value) * 100
         return value
     },
 
+    //Formata a data para como utilizamos no Brasil.
     formatDate(date) {
         if (date.indexOf('-') != -1) {
-            const splittedDate = date.split('-')
+            const splittedDate = /*Método para string que cria um array*/date.split('-')
             return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}` 
         } else {
             return date
         }
     },
     
+    //Formata o valor para o formato da moeda Brasileira (Real)
     formatCurrency(amount) {
         const signal = Number(amount) < 0 ? '-' : ''
     
         amount = this.formatAmount(amount)
-        amount = amount.toFixed(0)
-        amount = amount.toString().replace(/\D/g, '')
+        amount = amount.toFixed(0) //Método para fazer o arredondamento de casas decimais
+        amount = amount.toString().replace(/*expressão regular que captura todas as letras de uma string*//\D/g, '')
         amount = Number(amount) / 100
         amount = amount.toLocaleString('pt-br', {
             style: 'currency',
@@ -39,11 +42,12 @@ const Utils = {
         return signal + amount
     },
 
-    
+    //Identifica qual classe a transaction deverá receber, expanse para gastos e income para entradas.
     getExpanseOrIncome(transaction) {
         return transaction.amount < 0 ? 'expanse' : 'income'
     },
     
+    //Adiciona uma transaction ao DOM
     assignDOM(transaction, indice) {
         const area = document.querySelector('#area-transactions')
         const row = area.insertRow()
@@ -60,39 +64,45 @@ const Utils = {
     }
 }
 
-
+//Objeto da Transaction
 class Transaction {
     constructor(description, amount, date) {
-        //let id
         this.description = description
         this.amount = amount
         this.date = date
     }
 }
 
+//Objeto literal que armazena métodos que faz contanto com o localStorage, onde serão armazenadas as transactions
 const Storage = {
+    //recupera do banco de dados
     get() {
         return JSON.parse(localStorage.getItem('Transactions')) || []
     },
 
+    //Adiciona no banco de dados
     set(transactions) {
         localStorage.setItem('Transactions', JSON.stringify(transactions))
     }
 }
 
+//Objto literal que armazena uma array com todas as despesas e métodos para gerir o controle das despesas
 const manageTransactions = {
     all: Storage.get(),
 
+    //Adiciona uma nova transaction para a Array
     add(transaction) {
         this.all.push(transaction)
         App.reload()
     },
 
+    //Remove uma transaction da Array baseando-se no indice
     remove(indice) {
         this.all.splice(indice, 1)
         App.reload()
     },
 
+    //Faz a somátoria das entradas
     incomesValue() {
         let income = 0
 
@@ -105,6 +115,7 @@ const manageTransactions = {
         return income
     },
 
+    //Faz a somátoria dos gastos
     expansesValue() {
         let expanses = 0
 
@@ -117,21 +128,25 @@ const manageTransactions = {
         return expanses
     },
 
+    //Faz o calculo do total
     totalValue() {
         return this.incomesValue() + this.expansesValue()
     },
 
+    //Atualiza os cards
     updateBalance() {
         document.querySelector('#income').innerHTML = Utils.formatCurrency(this.incomesValue())
         document.querySelector('#expanses').innerHTML = Utils.formatCurrency(this.expansesValue())
         document.querySelector('#total').innerHTML = Utils.formatCurrency(this.totalValue())
     },
 
+    //Limpa o espaço das transações
     clearTransactions() {
         document.querySelector('#area-transactions').innerHTML = ''
     }
 }
 
+//Objeto literal que faz o controle total da aplicação
 const App = {
     init() {
         manageTransactions.all.forEach(Utils.assignDOM)
@@ -147,8 +162,10 @@ const App = {
     }
 }
 
+//chamada da função que incia o app
 App.init()
 
+//Função que captura o valor dos campos preenchidos do DOM
 function captureTransaction() {
     try {
         const description = document.querySelector('#description')
